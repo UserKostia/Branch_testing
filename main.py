ef3c43ba2eae31ceee97b9ca6568b5306756c67d
UserKostia <kostastriletski@gmail.com>
2024-10-15 19:37:38 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi_babel import Babel, BabelConfigs, _
+# from fastapi_babel.core import _
+
+configs = BabelConfigs(
+    ROOT_DIR=__file__,
+    BABEL_DEFAULT_LOCALE="en",
+    BABEL_TRANSLATION_DIRECTORY="lang",
+)
+babel = Babel(configs=configs)
+
+
+def main():
+    babel.locale = "en"
+    en_text = _("Hello World")
+    print(en_text)
+
+    babel.locale = "fa"
+    fa_text = _("Hello World")
+    print(fa_text)
+
+
+if __name__ == "__main__":
+    main()
